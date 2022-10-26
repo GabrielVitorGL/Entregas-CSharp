156557b5836106cd68261978ec7e117455e25e6f
Gabriel Vitor <84032696+GabrielVitorGL@users.noreply.github.com>
2022-10-26 10:59:38 -0300

    Add files via upload

--- Entregas SQL 2.0.sql
+++ Entregas SQL 2.0.sql
@@ -2,7 +2,7 @@ CREATE TABLE [moradores] (
   [id] int PRIMARY KEY IDENTITY(1, 1),
   [nome] nvarchar(255) NOT NULL,
   [cpf] nvarchar(255) UNIQUE NOT NULL,
-  [rg] varhar UNIQUE NOT NULL,
+  [rg] nvarchar(255) UNIQUE NOT NULL,
   [num_apto] nvarchar(255) NOT NULL,
   [bloco_apto] nvarchar(255) NOT NULL,
   [data_cadastro] datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP)
@@ -13,7 +13,7 @@ CREATE TABLE [encomendas] (
   [id] int PRIMARY KEY IDENTITY(1, 1),
   [id_morador] int,
   [data_cadastro] datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-  [0data_retirada] datetimed DEFAULT (CURRENT_TIMESTAMP),
+  [data_retirada] datetime DEFAULT (CURRENT_TIMESTAMP),
   [nome_recebedor] nvarchar(255)
 )
 GO
