fab310250808c763f37ffa17ab830a13ca45355f
Gabriel Vitor <84032696+GabrielVitorGL@users.noreply.github.com>
2022-10-26 10:58:15 -0300

    Add files via upload

---
+++ Entregas SQL 2.0.sql
@@ -0,0 +1,22 @@
+CREATE TABLE [moradores] (
+  [id] int PRIMARY KEY IDENTITY(1, 1),
+  [nome] nvarchar(255) NOT NULL,
+  [cpf] nvarchar(255) UNIQUE NOT NULL,
+  [rg] varhar UNIQUE NOT NULL,
+  [num_apto] nvarchar(255) NOT NULL,
+  [bloco_apto] nvarchar(255) NOT NULL,
+  [data_cadastro] datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+)
+GO
+
+CREATE TABLE [encomendas] (
+  [id] int PRIMARY KEY IDENTITY(1, 1),
+  [id_morador] int,
+  [data_cadastro] datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  [0data_retirada] datetimed DEFAULT (CURRENT_TIMESTAMP),
+  [nome_recebedor] nvarchar(255)
+)
+GO
+
+ALTER TABLE [encomendas] ADD FOREIGN KEY ([id_morador]) REFERENCES [moradores] ([id])
+GO
